2d03ef8e08e4a5e27e8f2eb84bd405ed38e458c3
Maximo Mayo <108980824+MaximoMayo@users.noreply.github.com>
2024-05-02 09:56:55 -0700

    Adding generator

---
+++ scripts/script.js
@@ -0,0 +1,34 @@
+function generateIcebreaker() {
+    const questions = [
+      "Sabrina Robles",
+      "Maximo Mayo",
+      "Niran Abdulqader",
+      "Jaranesca Braker",
+      "Moncerrat Lua Salas",
+      "Leslie Alvarado",
+      "Guadalupe Argumendo",
+      "Jesus Mimilia",
+      "Abdikadir",
+      "Alfi Ali",
+      "Brett Spokes",
+      "Daniel Solar",
+      "Dante Angerame",
+      "Kaaliyah Gray",
+      "Kadi Barry",
+      "Kayla Hanifen",
+      "Rene Banuelos",
+      "Uriel Delgado",
+      "Victor Mata Castaneda",
+      "Andrea Ceballos",
+    ];
+  
+    // Randomly select a question
+    const index = Math.floor(Math.random() * questions.length);
+  
+    document.getElementById("displayQuestion").innerHTML = `Hi ${questions[index]}`;
+    // return questions[index];
+  }
+  
+  // Example usage:
+  let thequestion = generateIcebreaker();
+  
